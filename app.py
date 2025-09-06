e5f526d95fd2a2aca521b10cd31f6f7cd65fc0ff
Kiranbanda2001 <kiran.a.banda@gmail.com>
2025-09-06 10:14:33 +0530

    Delete app.py

--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Cricbuzz Dashboard", layout="wide")
-st.title("🏏 Cricbuzz Dashboard")
-st.markdown("Use the sidebar to navigate between pages.")
